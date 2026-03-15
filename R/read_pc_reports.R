22fd78f4bd746679b5235750910cfc1668fc2714
davidsuson <susondavidimpo@gmail.com>
2026-03-15 22:31:00 +1100

    Added basic error handling

--- R/read_pc_reports.R
+++ R/read_pc_reports.R
@@ -7,28 +7,37 @@ read_pc_reports.data.frame <- function(reports){
 
   report_details <- apply(reports, 1, function(report){
 
-    report_name <- report["Name"]
-    report_link <- report["Link"]
+    tryCatch({
 
-    report_page <- paste0(report_link, "/report/")
-    submission_page <- paste0(report_link, "/submissions/")
+      report_name <- report["Name"]
+      report_link <- report["Link"]
 
-    report_released_date <- read_pc_report_details(report_page)
-    submissions <- read_pc_report_submissions(submission_page)
+      report_page <- paste0(report_link, "/report/")
+      submission_page <- paste0(report_link, "/submissions/")
 
-    final_report_details <- submissions %>%
-      dplyr::mutate(
-        Report_Name = report_name,
-        Report_Link = report_link,
-        Report_Released = report_released_date
-      )
+      report_released_date <- read_pc_report_details(report_page)
+      submissions <- read_pc_report_submissions(submission_page)
 
-    browser()
+      final_report_details <- submissions %>%
+        dplyr::mutate(
+          Report_Name = report_name,
+          Report_Link = report_link,
+          Report_Released = report_released_date
+        )
+
+
+    }, error = function(e) {
+
+      print(report)
+      return(data.frame())
+
+    })
+
+
 
   })
 
-  report_details
-
-
+  all_report_details <- report_details %>%
+    dplyr::bind_rows()
 
 }
